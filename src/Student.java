548e3ab1ab5bdbd0d0426ac95a1442e75bbab83b
dlanosparks <56090223+dlanosparks@users.noreply.github.com>
2019-10-30 15:41:17 -0700

    10/30 Class Update
    Comments

--- src/Student.java
+++ src/Student.java
@@ -12,6 +12,9 @@ public class Student extends Account_a {
 
     //Note that difficulty is not saved by this class: this will be handled dynamically within the game itself.
 
+    /**
+    * Constructor For Student Class. It holds Name, Username, Password, and Character Type
+    */
     public Student(String n, String u, String p, String ch)
     {
         super(n,u,p);
@@ -23,18 +26,22 @@ public class Student extends Account_a {
 
     //Getter Methods
 
+    // getter for HP
     public int getHP() {
         return HP;
     }
 
+    // getter for Attempted Answer
     public int getAnsAttempt() {
         return ansAttempt;
     }
 
+    // getter for Correct Answer
     public int getAnsCorrect() {
         return ansCorrect;
     }
 
+    // getter for Character Type
     public String getCharacter_type() {
         return character_type;
     }
@@ -42,25 +49,39 @@ public class Student extends Account_a {
     //Setter Methods
 
 
+    /** 
+    * setter for Attempted Answer
+    */
     public void setAnsAttempt(int ansAttempt) {
         this.ansAttempt = ansAttempt;
     }
 
+    /** 
+    * setter for Correct Answer
+    */
     public void setAnsCorrect(int ansCorrect) {
         this.ansCorrect = ansCorrect;
     }
 
+    /** 
+    * setter for Character Type
+    */
     //I don't expect us to need to use this, but in case a user decides to change their character type
     public void setCharacter_type(String character_type) {
         this.character_type = character_type;
     }
 
+    /** 
+    * setter for HP
+    */
     public void setHP(int HP) {
         this.HP = HP;
     }
 
 
-
+    /** 
+    * Returns a string Containing Name, Username, Password, HP, the Correct Answer, User's Attempted Answer, and User's Character Type
+    */
     @Override
     public String toString() {
         return (super.toString() + getHP() + "\n" + getAnsCorrect() + "\n" + getAnsAttempt() + "\n" + getCharacter_type());
